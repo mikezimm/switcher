375c08ec4344e4a0839e097079a9bbb9a26b3c2d
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-10-21 19:27:17 -0400

    Initial Yo

---
+++ src/webparts/switcher/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISwitcherWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SwitcherWebPartStrings' {
+  const strings: ISwitcherWebPartStrings;
+  export = strings;
+}
